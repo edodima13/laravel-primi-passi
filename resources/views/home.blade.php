ece71804b5563ca076aebb5ef3dd6b83ffe02036
edodima13 <edodima13@libero.it>
2022-04-27 17:22:42 +0200

    stampa dinamica e fine esercizio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-    <h1>
-        Hello World
-    </h1>
+    @php
+        $names = ['edoardo', 'simone', 'gianluca', 'filippo']
+    @endphp
+    @foreach ($names as $name)
+        <li>nome utente:{{ $name }}</li>
+    @endforeach
 </body>
 </html>
